7907707c5fed78717e1d8fb7649de34da6e994c2
gbaldass <mike_baldassare@yahoo.com>
2017-09-21 15:25:52 -0600

    push

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,12 +1,15 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+app.get('/windows_testing', function(req, res) {
+  res.sendFile(path.join(__dirname, '../src/windows_testing.ova'));
 });
 
 app.listen(port, function(err) {
